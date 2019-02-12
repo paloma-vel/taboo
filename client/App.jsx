8624106b7d857380cd93c3d1bb2116e9448d50d5
Paloma Velazquez <paloma.velazquez@gmail.com>
2019-02-11 16:15:38 -0800

    Implement team point counter

--- client/App.jsx
+++ client/App.jsx
@@ -12,33 +12,65 @@ class App extends React.Component {
       scoreA: 0,
       scoreB: 0
     }
+    this.addPointA = this.addPointA.bind(this);
+    this.addPointB = this.addPointB.bind(this);
+    this.subtractPointA = this.subtractPointA.bind(this);
+    this.subtractPointB = this.subtractPointB.bind(this);
+
+  }
+
+  addPointA() {
+    this.setState({scoreA: this.state.scoreA + 1})
+  }
+
+  addPointB() {
+    this.setState({scoreB: this.state.scoreB + 1})
+  }
+
+  subtractPointA() {
+    if (this.state.scoreA > 0) {
+      this.setState({scoreA: this.state.scoreA - 1})
+    }
+  }
+
+  subtractPointB() {
+    if (this.state.scoreB > 0) {
+      this.setState({scoreB: this.state.scoreB - 1})
+    }
   }
 
   render() {
     return (
       <div id="container">
         <div className="intro">
-          <h1>Taboo</h1>
-          <i className="how-to-play far fa-question-circle"></i>
+            <h1>Taboo</h1>
+            <i className="how-to-play far fa-question-circle"></i>
         </div>
         <div className="row">
-        <div className="col">
-          <Card />
-          <div className="change-card">
-            <i className="next-card far fa-arrow-alt-circle-right"></i>
-          </div>
-        </div>
-        <div className="col">
-        <div className="timer">
-          <span className="countdown">1:00</span>
-            <div className="timer-buttons">
-              <i className="fas fa-play"></i>
-              <i className="fas fa-stop"></i>
-              <i className="fas fa-undo-alt"></i>
+            <div className="col">
+              <Card />
+              <div className="change-card">
+                  <i className="next-card far fa-arrow-alt-circle-right"></i>
+              </div>
             </div>
-        </div>
-          <Score />
-        </div>
+            <div className="col">
+              <div className="timer">
+                  <span className="countdown">1:00</span>
+                  <div className="timer-buttons">
+                    <i className="fas fa-play"></i>
+                    <i className="fas fa-stop"></i>
+                    <i className="fas fa-undo-alt"></i>
+                  </div>
+              </div>
+              <Score
+                scoreA={this.state.scoreA}
+                scoreB={this.state.scoreB}
+                addPointA={this.addPointA}
+                addPointB={this.addPointB}
+                subtractPointA={this.subtractPointA}
+                subtractPointB={this.subtractPointB}
+              />
+            </div>
         </div>
       </div>
     )
--- client/Score.jsx
+++ client/Score.jsx
@@ -10,16 +10,18 @@ class Score extends React.Component {
       <div className="score">
         <div className="score-container">
           <span className="team">Team A</span>
-          <span className="each-score">0</span>
+          <span className="each-score">{this.props.scoreA}</span>
           <div className="score-buttons">
-            <i className="subtract-point far fa-minus-square"></i><i className="add-point far fa-plus-square"></i>
+            <i className="subtract-point far fa-minus-square" onClick={this.props.subtractPointA}></i>
+            <i className="add-point far fa-plus-square" onClick={this.props.addPointA}></i>
           </div>
         </div>
         <div className="score-container">
           <span className="team">Team B</span>
-          <span className="each-score">0</span>
+          <span className="each-score">{this.props.scoreB}</span>
           <div className="score-buttons">
-            <i className="subtract-point far fa-minus-square"></i><i className="add-point far fa-plus-square"></i>
+            <i className="subtract-point far fa-minus-square" onClick={this.props.subtractPointB}></i>
+            <i className="add-point far fa-plus-square" onClick={this.props.addPointB}></i>
           </div>
         </div>
       </div>
